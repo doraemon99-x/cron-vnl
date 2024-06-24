8372740102cc718769d26dec4def5f02b6da4232
DoraemonX <75873265+doraemon99-x@users.noreply.github.com>
2024-06-24 19:28:16 +0700

    Update vnl.py

--- vnl.py
+++ vnl.py
@@ -24,7 +24,7 @@ def schedule_job_on_specific_date():
     wib = pytz.timezone('Asia/Jakarta')
     
     # Tanggal target
-    target_date = datetime(2024, 6, 24, tzinfo=wib)
+    target_date = datetime(2024, 6, 25, tzinfo=wib)
 
     # Daftar waktu target dalam satu hari
     target_times = [
